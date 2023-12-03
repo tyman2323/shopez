b5254a583a2edfdd16e676dcbdda01428d71864f
tyman2323 <122325523+tyman2323@users.noreply.github.com>
2023-12-03 12:36:40 -0500

    ch

---
+++ lib/screens/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:projecttwo/screens/homescreen.dart';
+import 'package:projecttwo/screens/splashscreen.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: splashscreen(),
+      routes: {
+        '/homescreen': (context) => homescreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:projecttwo/screens/homescreen.dart';
+
+class splashscreen extends StatefulWidget {
+  @override
+  State<splashscreen> createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigatetohome();
+  }
+
+  navigatetohome() async {
+    await Future.delayed(
+      Duration(seconds: 3),
+    );
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => homescreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text(
+                'You\'re about to enter ShopEz',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(8),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: SizedBox.fromSize(
+                  size: Size.fromRadius(150), // Image radius
+                  child: Image.network(
+                      'https://media.tenor.com/YjuxAL4URQ0AAAAC/floating-shopping-cart.gif',
+                      fit: BoxFit.cover),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
