88c26c617359163f26b1cdb5c6509aa2dfc4bf7d
Pau Tuang <124294335+ptuang@users.noreply.github.com>
2023-12-09 04:01:34 -0500

    Update productscreen.dart
    //include necessary dependencies

--- lib/screens/productscreen.dart
+++ lib/screens/productscreen.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:intl/intl.dart';
 
 class ProductScreen extends StatefulWidget {
   const ProductScreen({Key? key, required this.auth, required this.userDocSnap, required this.productSnap})
@@ -15,163 +17,338 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
-  //reference to products collection
-  final CollectionReference _productss =
-  FirebaseFirestore.instance.collection('products');
-
-  //reference to user collection - will use this to get count of product in cart if exists
-  final CollectionReference _userss =
-  FirebaseFirestore.instance.collection('users');
-
-  //increase product count
-  Future<void> _increaseProduct(String productId) async {
-    final CollectionReference currentCart = FirebaseFirestore.instance
-        .collection('users')
-        .doc(widget.userDocSnap.id)
-        .collection('current_cart');
-    DocumentReference docRef = currentCart.doc('products_$productId');
-    if (docRef != null) {
-      await docRef.update({'count': FieldValue.increment(1)});
-    } else {
-      await currentCart.doc('products_$productId').set({'count': 1});
-    }
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold for the Product Screen
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Details'),
+      ),
+      body: ProductDetails(
+        productSnap: widget.productSnap,
+        userDocSnap: widget.userDocSnap,
+      ),
+    );
   }
+}
 
-  //decrease product count
-  Future<void> _decreaseProduct(String productId) async {
-    final CollectionReference currentCart = FirebaseFirestore.instance
-        .collection('users')
-        .doc(widget.userDocSnap.id)
-        .collection('current_cart');
-    DocumentReference docRef = currentCart.doc('products_$productId');
-    if (docRef != null) {
-      DocumentSnapshot docSnap = await docRef.get();
-      if (docSnap['count'] == 1) {
-        await currentCart.doc(docSnap.id).delete();
-      }
-    }
-    await docRef.update({'count': FieldValue.increment(-1)});
+class ProductDetails extends StatelessWidget {
+  const ProductDetails({Key? key, required this.productSnap, required this.userDocSnap})
+      : super(key: key);
+
+  final DocumentSnapshot userDocSnap;
+  final DocumentSnapshot productSnap;
+
+  @override
+  Widget build(BuildContext context) {
+    // Container with product details
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Product header
+            Center(
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                child: ProductHeader(productSnap: productSnap),
+              ),
+            ),
+            // Product image
+            Center(
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                child: ProductImage(productSnap: productSnap),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Product information
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: ProductInfo(productSnap: productSnap),
+            ),
+            const SizedBox(height: 20),
+            // Button to show reviews
+            ElevatedButton(
+              onPressed: () {
+                // Show modal bottom sheet with product reviews
+                showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext ctx) => ProductReviewsModal(productSnap: productSnap),
+                );
+              },
+              child: const Text('Reviews'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
 
+class ProductHeader extends StatelessWidget {
+  const ProductHeader({Key? key, required this.productSnap}) : super(key: key);
 
-  ///because this is a function... when the arrow button on a specific product
-  ///is pressed on the home screen, I would call this function sending in the chosen products information and a bottom
-  ///modal will pop up with the information displayed
-  Future<void> _addUserReview(String review) async {
-    CollectionReference userReviews = _productss.doc(widget.productSnap.id).collection('userreviews');
+  final DocumentSnapshot productSnap;
 
-    var name = widget.userDocSnap['firstname'] + ' ' + widget.userDocSnap['lastname'];
-    userReviews.add({'uid' : widget.userDocSnap.id, 'username' : name, 'review' : review, 'post_time' : Timestamp.fromDate(DateTime.now())});
+  @override
+  Widget build(BuildContext context) {
+    // Product name text
+    return Text(
+      productSnap['product-name'],
+      style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+    );
   }
+}
 
-  Future<void> _productReviewsModalSheet([DocumentSnapshot? documentSnapshot]) async {
+class ProductImage extends StatelessWidget {
+  const ProductImage({Key? key, required this.productSnap}) : super(key: key);
 
-    //this is a reference to the specified products list of user reviews
-    CollectionReference userReviews = _productss.doc(documentSnapshot?.id).collection('userreviews');
+  final DocumentSnapshot productSnap;
 
-    await showModalBottomSheet(
-        isScrollControlled: true,
-        context: context,
-        builder: (BuildContext ctx) {
-          return Padding(
-            padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
+  @override
+  Widget build(BuildContext context) {
+    // Product image widget
+    var productImageUrl = productSnap['product-img-url'] ?? '';
+
+    return Image.network(
+      productImageUrl,
+      height: 500,
+      width: 500,
+    );
+  }
+}
+
+class ProductInfo extends StatelessWidget {
+  const ProductInfo({Key? key, required this.productSnap}) : super(key: key);
+
+  final DocumentSnapshot productSnap;
+
+  @override
+  Widget build(BuildContext context) {
+    // Container with product information
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Row with price and rating
+          Container(
+            margin: const EdgeInsets.only(bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('${documentSnapshot?['product-name']} Reviews'),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: (){
-                      //add text field to insert review into user_reviews collection
-                      // call _addUserReview(review) to add the review to the database on submit
-                    },
-                    child: const Text('Add Review')),
-                Flexible(
-                    child: StreamBuilder( //use to display user reviews; display user name and review and date
-                      stream: userReviews.snapshots(),
-                      builder: (BuildContext context, AsyncSnapshot<dynamic> streamSnapshot) {
-                        if (streamSnapshot.hasData) {
-                          return ListView.builder(
-                            itemCount: streamSnapshot.data!.docs.length,
-                            itemBuilder: (context, index) {
-                              final DocumentSnapshot documentSnapshot = streamSnapshot.data!.docs[index];
-                              return Card(
-                                margin: const EdgeInsets.all(10),
-                                child: ListTile(
-                                  title: Text('Put the review here'), //add their name too... and maybe the date
-                                ),
-                              );
-                            },
-                          );
-                        }
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
+                Text('Price: \$${productSnap['product-price']}'),
+                // Row with rating stars
+                Row(
+                  children: [
+                    Text('Rating: ${productSnap['product-rating']}'),
+                    const SizedBox(width: 5),
+                    RatingBar.builder(
+                      initialRating: double.parse(productSnap['product-rating'].toString()),
+                      minRating: 1,
+                      direction: Axis.horizontal,
+                      allowHalfRating: true,
+                      itemCount: 5,
+                      itemSize: 20,
+                      itemPadding: const EdgeInsets.symmetric(horizontal: 2.0),
+                      itemBuilder: (context, _) => const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {
+                        // ignore this part, rating won't be updated
                       },
                     ),
+                  ],
                 ),
               ],
             ),
-          );
-        });
+          ),
+          // Product description
+          Container(
+            margin: const EdgeInsets.only(bottom: 10),
+            child: Text('Product Description: ${productSnap['product-description']}'),
+          ),
+        ],
+      ),
+    );
   }
+}
 
+class ProductReviewsModal extends StatelessWidget {
+  const ProductReviewsModal({Key? key, required this.productSnap}) : super(key: key);
 
+  final DocumentSnapshot productSnap;
 
-  /// This will be inside of a showModalBottomSheet Function
   @override
   Widget build(BuildContext context) {
-
-    String product_name = '';
-    var product_imag_url = '';
-    var product_price = '';
-    var product_rating = '';
-    var product_desc = '';
-    if (widget.productSnap != null) {
-      product_name = widget.productSnap['product-name'];
-      //product_imag_url = widget.productSnap['product-img'];
-      // I believe you made this an array in the database, so you can figure out a way to acces the image url
-      // for now i will leave the url as an empty string
-      product_price = widget.productSnap['product-price'].toString();
-      product_rating = widget.productSnap['product-rating'].toString();
-      product_desc = widget.productSnap['product-description'];
-    }
-
-    return Padding(
+    // Container with product reviews modal
+    return Container(
       padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: MediaQuery.of(context).viewInsets.bottom + 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(product_name, style: const TextStyle(fontWeight: FontWeight.bold),),
-          const SizedBox(
-            height: 20,
-          ),
-          Image.asset(product_imag_url, height: 300, width: 300),
-          const SizedBox(height: 20),
-          Text('Price: \$$product_price'),
-          const SizedBox(height: 20),
-          Text('Average Rating: $product_rating'),
-          const SizedBox(height: 20),
-          Text('Product Description: $product_desc'),
-          const SizedBox(height: 20),
+          // Title for product reviews
+          Text('${productSnap['product-name']} Reviews', style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 10),
+          // Button to add a review
           ElevatedButton(
-            //you can use this button to show/build the user reviews, it would make the page longer
-            //you can either make display them on the same page or navigate to another page to display it
-            //on that page or modal sheet you can have another button that allows the user to add their review
-            //use this button to pull up a text field that allows a user to input a review of the product
-            //review is made of two parts - rating a text; it can be just a rating or both
-            //I was looking at the walmart app and they use a bottomModalSheet to display a specific product
-            //and clicking the user reviews pulls up another page in the modal. you can do it like that
-
-              onPressed: () {
-                _productReviewsModalSheet(); //this calls the function that displays modal with user reviews
-              },
-              child: const Text('Reviews'))
-
+            onPressed: () {
+              // Open a bottom sheet for adding reviews
+              _addReview(context);
+            },
+            child: const Text('Add Review'),
+          ),
+          // List of product reviews
+          ProductReviewList(productSnap: productSnap),
         ],
       ),
     );
+  }
+
+  // Function to open the bottom sheet for adding reviews
+  void _addReview(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext ctx) {
+        return _AddReviewForm(productSnap: productSnap);
+      },
+    );
+  }
+}
+
+class _AddReviewForm extends StatefulWidget {
+  const _AddReviewForm({Key? key, required this.productSnap}) : super(key: key);
+
+  final DocumentSnapshot productSnap;
+
+  @override
+  State<_AddReviewForm> createState() => _AddReviewFormState();
+}
+
+class _AddReviewFormState extends State<_AddReviewForm> {
+  final TextEditingController _reviewController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // Container with form to add a review
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Title for the add review form
+          Text('Add Review', style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 10),
+          // TextField for the review text
+          TextField(
+            controller: _reviewController,
+            decoration: const InputDecoration(labelText: 'Write your review'),
+          ),
+          const SizedBox(height: 10),
+          // Button to submit the review
+          ElevatedButton(
+            onPressed: () {
+              // Call a function to add the review to the Firestore collection
+              _addUserReview(widget.productSnap.id, _reviewController.text);
+              // Close the bottom sheet after adding the review
+              Navigator.of(context).pop();
+            },
+            child: const Text('Submit Review'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Function to add the user review to the Firestore collection
+  void _addUserReview(String productId, String review) {
+    CollectionReference userReviews = FirebaseFirestore.instance.collection('review');
+    userReviews.add({
+      'uid': FirebaseAuth.instance.currentUser?.uid ?? '',
+      'user-review': review,
+      'firstname': 'anonymous',
+      'time': DateTime.now().millisecondsSinceEpoch,
+      'product-id': productId,
+    });
+  }
+}
+
+class ProductReviewList extends StatelessWidget {
+  const ProductReviewList({Key? key, required this.productSnap}) : super(key: key);
+
+  final DocumentSnapshot productSnap;
+
+  @override
+  Widget build(BuildContext context) {
+    // Firestore collection reference for user reviews
+    CollectionReference userReviews = FirebaseFirestore.instance.collection('review');
+
+    return Flexible(
+      child: StreamBuilder(
+        stream: userReviews.where('product-id', isEqualTo: productSnap.id).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            // List view of user reviews
+            return ListView.builder(
+              itemCount: streamSnapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot documentSnapshot = streamSnapshot.data!.docs[index];
+                // Card with user review details
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    title: Text(documentSnapshot['user-review']),
+                    subtitle: Text('${documentSnapshot['firstname']} - ${_formatTime(documentSnapshot['time'])}'),
+                  ),
+                );
+              },
+            );
+          }
+          // Loading indicator while data is being fetched
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+
+  // Helper function to format the timestamp to a readable format
+  String _formatTime(int timestamp) {
+    final DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(timestamp);
+    return DateFormat('MM/dd/yyyy HH:mm').format(dateTime);
   }
 }
