ba2ae961c670ec9200ecfa5ac912a1a00705c7f6
Onyinyechukwu Onyemaobi <101658048+onyinye2@users.noreply.github.com>
2023-12-09 19:29:41 -0500

    feat-onyi: home screen search bar working

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -18,14 +20,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 // text fields' controllers
-  final SearchController _searchController = SearchController(); //textediting?
+
+  final TextEditingController searchTextController = TextEditingController();
 
   final CollectionReference _productss =
       FirebaseFirestore.instance.collection('products');
 
   final CollectionReference _userss =
       FirebaseFirestore.instance.collection('users');
-
 
   Future<void> _increaseProduct(String productId) async {
     final CollectionReference currentCart = FirebaseFirestore.instance
@@ -35,6 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
     DocumentReference docRef = currentCart.doc('products_$productId');
     if (docRef != null) {
       await docRef.update({'count': FieldValue.increment(1)});
+      //await currentCart.add({'count' : 1, 'price' : productSnap['price']});
     } else {
       await currentCart.doc('products_$productId').set({'count': 1});
     }
@@ -55,8 +58,39 @@ class _HomeScreenState extends State<HomeScreen> {
     await docRef.update({'count': FieldValue.increment(-1)});
   }
 
+  Future<void> _addProductToCart(DocumentSnapshot productSnap) async {
+    String productId = productSnap.id;
+    final CollectionReference currentCart = FirebaseFirestore.instance
+        .collection('users')
+        .doc(widget.userDocSnap.id)
+        .collection('current_cart');
+    DocumentSnapshot temp = await currentCart.doc('products_$productId').get();
+    if (temp.exists) {
+      currentCart.doc('products_$productId').delete();
+    } else {
+      currentCart.doc('products_$productId').set({
+        "count": 1,
+        'name': productSnap['product-name'],
+        'price': productSnap['product-price']
+      }).onError((e, _) => print("Error writing document: $e"));
+      ;
+    }
+  }
+
+  //Future<void> addToCart
+  Widget productList = Container(
+    child: const Text('No matching products'),
+  );
+  void rebuildProductList(String str) {
+    setState(() {
+      productList = buildProductList(str);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    productList = buildProductList(searchTextController.text);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shop EZ'),
@@ -80,7 +114,8 @@ class _HomeScreenState extends State<HomeScreen> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (BuildContext context) => SettingsScreen(auth: widget.auth, userDocSnap: widget.userDocSnap),
+                  builder: (BuildContext context) => SettingsScreen(
+                      auth: widget.auth, userDocSnap: widget.userDocSnap),
                 ),
               );
             },
@@ -93,96 +128,91 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            Padding(padding: const EdgeInsets.all(10), child: SizedBox(height: 60, child: buildSearchBar())),
-            buildProductList(_searchController.text),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: SizedBox(
+                height: 60,
+                child: Flexible(
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 400,
+                        child: TextField(
+                          controller: searchTextController,
+                          decoration:
+                              const InputDecoration(hintText: 'Enter Search'),
+                          onSubmitted: (String str) {
+                            setState(() {
+                              productList =
+                                  buildProductList(searchTextController.text);
+                            });
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 15),
+                      ElevatedButton(
+                        onPressed: () {
+                          rebuildProductList(searchTextController.text);
+                          //searchTextController.text = '';
+                        },
+                        child: const Text('Load Product List'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            productList,
           ],
         ),
       ),
     );
   }
 
-  Widget buildSearchBar() {
-    //bool isDark = false;
-    //final ThemeData themeData = ThemeData(useMaterial3: true, brightness: isDark ? Brightness.dark : Brightness.light);
-
-    return SearchAnchor(
-        builder: (BuildContext context, SearchController controller) {
-      return SearchBar(
-        controller: _searchController,
-        padding: const MaterialStatePropertyAll<EdgeInsets>(
-            EdgeInsets.symmetric(horizontal: 16.0)),
-        onTap: () {
-          _searchController.openView();
-        },
-        onChanged: (_) {
-          _searchController.openView();
-        },
-        leading: const Icon(Icons.search),
-        /*trailing: <Widget>[
-              Tooltip(message: 'Change brightness mode', child: IconButton(
-                  isSelected: isDark,
-                  onPressed: () {
-                    setState(() {
-                      isDark = !isDark;
-                    });
-                  },
-                  icon: const Icon(Icons.wb_sunny_outlined),
-                  selectedIcon: const Icon(Icons.brightness_2_outlined),
-                ),
-              )
-            ],*/
-      );
-    }, suggestionsBuilder: (BuildContext context, SearchController controller) {
-      return List<ListTile>.generate(5, (int index) {
-        final String item = 'item $index';
-        return ListTile(
-          title: Text(item),
-          onTap: () {
-            setState(() {
-              _searchController.closeView(item);
-            });
-          },
-        );
-      });
-    });
-  }
-
   Future<void> _productScreen(DocumentSnapshot prodSnap) async {
-
     await showModalBottomSheet(
-      isScrollControlled: true,
-      context: context,
-      builder: (BuildContext ctx) {
-        return ProductScreen(auth: widget.auth, userDocSnap: widget.userDocSnap, productSnap: prodSnap);
-      }
-    );
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return ProductScreen(
+              auth: widget.auth,
+              userDocSnap: widget.userDocSnap,
+              productSnap: prodSnap);
+        });
   }
 
   Widget buildProductList(String searchText) {
     return Flexible(
       child: StreamBuilder(
-        stream: searchText == '' ? _productss.snapshots() : _productss.where('product-name', isEqualTo: searchText).snapshots(),
+        stream: searchText == ''
+            ? _productss.snapshots()
+            : _productss
+                .where('product-name', isEqualTo: searchText)
+                .snapshots(),
         builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
           if (streamSnapshot.hasData) {
             return ListView.builder(
               itemCount: streamSnapshot.data!.docs.length,
               itemBuilder: (context, index) {
-                final DocumentSnapshot documentSnapshot = streamSnapshot.data!.docs[index];
+                final DocumentSnapshot documentSnapshot =
+                    streamSnapshot.data!.docs[index];
+                bool temp = true;
                 return Card(
                   margin: const EdgeInsets.all(10),
                   child: ListTile(
                     tileColor: Colors.grey,
-                    leading: Image.asset(
-                      documentSnapshot['product-img'][0].toString(),
+                    leading: const Icon(Icons.image),
+                    /*Image.network(
+                      documentSnapshot['product-img-url'].toString(),
                       height: 25,
                       width: 25,
-                    ),
+                    ),*/
                     title: Text(documentSnapshot['product-name']),
                     trailing: SizedBox(
-                      width: 175,
+                      width: 125,
                       child: Row(
                         children: [
-                          IconButton(
+                          /*IconButton(
                               icon: const Icon(
                                 Icons.remove,
                                 color: Colors.white70,
@@ -190,14 +220,16 @@ class _HomeScreenState extends State<HomeScreen> {
                               onPressed: () =>
                                   _decreaseProduct(documentSnapshot.id)),
                           countText(documentSnapshot.id), //i am having issues accessing user current cart doc.
+                          */
                           IconButton(
                               icon: const Icon(
                                 Icons.add,
                                 color: Colors.white70,
                               ),
-                              onPressed: () =>
-                                  _increaseProduct(documentSnapshot.id)
-                          ),
+                              onPressed: () {
+                                temp = !temp;
+                                _addProductToCart(documentSnapshot);
+                              }),
                           IconButton(
                               onPressed: () {
                                 /*Navigator.push(
@@ -208,10 +240,11 @@ class _HomeScreenState extends State<HomeScreen> {
                                         userDocSnap: widget.userDocSnap), //change to docref
                                   ),
                                 );*/
-                                _productScreen(documentSnapshot); //- call this when product screen function is done
+                                _productScreen(
+                                    documentSnapshot); //- call this when product screen function is done
                               },
-                              icon: const Icon(Icons.arrow_forward, color: Colors.white70)
-                          )
+                              icon: const Icon(Icons.arrow_forward,
+                                  color: Colors.white70))
                         ],
                       ),
                     ),
@@ -227,33 +260,33 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-  
-  
 
-  Widget countText(String productId){
-    final CollectionReference currentCart = _userss.doc(widget.userDocSnap.id).collection('current_cart');
-    
+  Widget countText(String productId) {
+    final CollectionReference currentCart =
+        _userss.doc(widget.userDocSnap.id).collection('current_cart');
+
     return FutureBuilder(
       future: currentCart.doc('products_$productId').get(),
-      builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          //Error Handling conditions
-          if (snapshot.hasError) {
-            return const Text("Something went wrong");
-          }
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        //Error Handling conditions
+        if (snapshot.hasError) {
+          return const Text("Something went wrong");
+        }
 
-          if (snapshot.hasData && !snapshot.data!.exists) {
-            return const Text("0");
-          }
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return const Text("0");
+        }
 
-          //Data is output to the user
-          if (snapshot.connectionState == ConnectionState.done) {
-            Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
-            return Text("${data['count']}");
-          }
-          
-          return const Text('Loading');
+        //Data is output to the user
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          return Text("${data['count']}");
+        }
 
-        },
+        return const Text('Loading');
+      },
     );
   }
 }
